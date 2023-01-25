05ae81ac7ef7bd0c32bbf95a0a175cb01c359ef6
Adam E Nagy <adam_e_nagy@hotmail.com>
2023-01-25 13:16:46 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.use(session({
     proxy: true,
     cookie: {
         httpOnly: true,
-        secure: (process.env.NODE_ENV === 'production'),
+        secure: false,//(process.env.NODE_ENV === 'production'),
         maxAge: 1000 * 60 * 60 // 1 hours to expire the session and avoid memory leak
     },
     resave: false,
